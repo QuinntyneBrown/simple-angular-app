67da3f17324d40d4ffef65780a5fed78f990ebba
Quinntyne Brown <quinntynebrown@gmail.com>
2017-06-26 21:04:13 -0400

    egghead tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component, ViewEncapsulation, trigger, state, style } from "@angular/core";
+import { Component, ViewEncapsulation, trigger, state, style, transition, animate } from "@angular/core";
 
 @Component({
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"],
     selector: "app",    
+    encapsulation: ViewEncapsulation.Emulated,
     animations: [
         trigger('signal', [
             state('go', style({
-                'background-color':'green'
-            }))
+                'background-color': 'green'
+            })),
+            state('stop', style({
+                'background-color': 'red'
+            })),
+            transition('* => *', animate(500))
         ]
-    )],
-    encapsulation: ViewEncapsulation.Emulated
+    )]
 })
-export class AppComponent { }
+export class AppComponent {
+    public signal: string = 'stop';
+
+    public onGoClick() {
+        this.signal = 'go';
+    }
+
+    public onStopClick() {
+        this.signal = 'stop';
+    }
+}
